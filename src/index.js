c35becbbfebd8a11cdc949a082368d42be8afcf7
Sohlina <a.sohlin@hotmail.com>
2021-04-11 16:04:06 +0200

    Cleaning JS structure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
 
-
   let days = [
     "Sunday",
     "Monday",
@@ -25,27 +24,21 @@ function formatDate(timestamp) {
 
 function showCurrentTemperature (response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
-
-
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
-
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
-
   let windElement = document.querySelector("#wind-speed");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-
   let dateElement = document.querySelector("#date");
+  
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "36362df01ab1fd13299eeea7914024b2";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=${apiKey}&units=metric`; 
-console.log(apiUrl);
 
 axios.get(apiUrl).then(showCurrentTemperature);
